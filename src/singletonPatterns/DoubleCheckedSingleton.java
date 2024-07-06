350800073078e30d4e43f2f1a2a29cb306d9287c
Naveen <naveenkant227@gmail.com>
2024-07-06 23:19:30 +0530

    added singletom pattern types

---
+++ src/singletonPatterns/DoubleCheckedSingleton.java
@@ -0,0 +1,27 @@
+package singletonPatterns;
+
+public class DoubleCheckedSingleton {
+    // The single instance, initially null, marked as volatile
+    private static volatile DoubleCheckedSingleton instance;
+    private final static Object lock = new Object();
+
+    // Private constructor to prevent instantiation
+    private DoubleCheckedSingleton() {}
+
+    // Public method to get the instance
+    public static DoubleCheckedSingleton getInstance() {
+        // First check (not synchronized)
+        if (instance == null) {
+            // Synchronize on the class object
+            synchronized(lock) {
+                // Second check (synchronized)
+                if (instance == null) {
+                    // Create the instance
+                    instance = new DoubleCheckedSingleton();
+                }
+            }
+        }
+        // Return the instance (either newly created or existing)
+        return instance;
+    }
+}
---
+++ src/singletonPatterns/EagerSingleton.java
@@ -0,0 +1,23 @@
+package singletonPatterns;
+
+public class EagerSingleton {
+    // The single instance, created immediately
+    private static final EagerSingleton instance = new EagerSingleton();
+
+    // Private constructor to prevent instantiation
+    private EagerSingleton() {}
+
+    // Public method to get the instance
+    public static EagerSingleton getInstance() {
+        return instance;
+    }
+}
+
+
+/*Summary
+Eager initialization is simple and thread-safe but can be resource-intensive and inflexible.
+ It's not suitable for all situations, particularly when:
+
+The singleton is resource-heavy or requires runtime parameters.
+You want to delay the instance creation until it's actually needed (lazy loading).
+The application startup time is critical, and you want to avoid any unnecessary overhead during the startup phase.*/
