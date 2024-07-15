31b4d1b630dcb73586e64e690ce94d5075ddd249
Naveen Kumar <naveenkant227@gmail.com>
2024-07-16 00:16:29 +0530

    added decorator pattern

---
+++ src/decoratorPattern/Jalpeeno.java
@@ -0,0 +1,12 @@
+package decoratorPattern;
+
+public class Jalpeeno extends ToppingDecorator{
+    BasePizza basePizza;
+    public Jalpeeno(BasePizza basePizza) {
+        this.basePizza = basePizza;
+    }
+    @Override
+    public int cost() {
+        return basePizza.cost()+30;
+    }
+}
